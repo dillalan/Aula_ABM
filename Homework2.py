79efb8c5da8a21fe8ab25804765638e6c822aa1d
Alan Dill <alandill@rocketmail.com>
2020-06-05 20:33:11 -0300

    A Guessing Game

---
+++ Homework2.py
@@ -0,0 +1,46 @@
+"""A function that compare a number guessed by the user with the actual hidden a number between 0 to 100. Hints are
+provided. """
+import random
+
+
+def hint(t, ans):
+    if t > ans:
+        print(f"\n<<<<<<<<<<<<<< HINT >>>>>>>>>>>>>> \nA resposta certa é menor do que {t}")
+    else:
+        print(f"\n<<<<<<<<<<<<<< HINT >>>>>>>>>>>>>> \nA resposta certa é maior do que {t}")
+
+
+def test_guess(ans, count):
+    t = int(input("\nDigite um número de 0 à 100\n:"))
+    if t == ans:
+        print(
+            f" \n#### A C E R T O U ! #### \n\n@@@@ P A R A B É N S @@@ \n\nDepois de {count} tentativas ficou fácil, "
+            f"né!?")
+        return False
+    if t != ans:
+        if count > 3:
+            hint(t, ans)
+            count += 1
+            print('\nPreste atenção na dica e tente de novo!')
+        else:
+            print("\nNope! Tente mais uma vez!")
+            count += 1
+        return True
+
+
+def user_int():
+    print("Olá! Bem vindo ao jogo da adivinhação!\n\nEu escolho um número e você tenta adivinhar")
+    r = input("Vamos começar? \n\nPressione: (1) SIM, ou (0)NÃO\n:")
+    if r == '1':
+        ans = random.randint(0, 100)
+        count = 0
+        infinite = True
+        while infinite:
+            count += 1
+            infinite = test_guess(ans, count)
+    elif r == '0':
+        return False
+
+
+if __name__ == '__main__':
+    user_int()
